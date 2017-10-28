1cb6329fee2458bb3963c03adc2b26de1d3651fc
Blakeley Bond <burrick2003@yahoo.com>
2017-10-28 03:25:01 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -10,7 +10,6 @@ end
 
 
 def not_safe?(speed)
-	speed > 60 ? true :
-	speed < 40 ? true :
-	speed >= 40 && speed <= 60 ? false :
+	speed > 60 ? true :	speed < 40 ? true :
+	speed >= 40 && speed <= 60 ? false : nil
 end
